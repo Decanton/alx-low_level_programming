436b3baefa45280cfc85fc15f06c4900bd6dde56
decanton <owuordickens@gmail.com>
2023-02-16 04:15:42 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistid.h>
+#include <unistd.h>
 /**
- * main - a program that prints a line followed by new line
- * Description: to astandard error
+ * main - a program that prints a text to a standard error
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
