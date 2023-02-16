46bc6ac5650bf982dce6a7bc3510bd047587e087
decanton <owuordickens@gmail.com>
2023-02-16 04:03:29 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistid.h>
+/**
+ * main - a program that prints a line followed by new line
+ * Description: to astandard error
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
