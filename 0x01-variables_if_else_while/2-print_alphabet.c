3aa4d3ad1950e9059240a38d5ece79e91b8f3c2d
decanton <owuordickens@gmail.com>
2023-02-17 01:09:51 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - write alphabet in lowercase
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char aph;
+
+	for (aph = 'a'; aph <= 'z'; aph++)
+	{
+		putchar(aph);
+	}
+	putchar("\n");
+	return (0);
+}
