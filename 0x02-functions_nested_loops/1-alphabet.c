fdcc931a4f786946c0d1f86ea9cff6e3dd3d2c20
decanton <owuordickens@gmail.com>
2023-02-20 22:29:57 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a function that prints alphabet in lowercase
+ * Return: 0 Always
+ */
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
