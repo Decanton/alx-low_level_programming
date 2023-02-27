f7c417b7d93549dd9476d8ec830b765c4681ef59
decanton <owuordickens@gmail.com>
2023-02-27 04:52:23 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int k = 0;
+
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+	if (i % 2 == 1)
+	{
+	i = (i - 1) / 2;
+	k += 1;
+	}
+	else
+	{
+	k = i / 2;
+	}
+
+	for (; k < i; k++)
+	{
+	_putchar(str[k]);
+	}
+	_putchar('\n');
+}
+
