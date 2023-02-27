ed6ff16063b6796a73b31ed7905069a7c5f6ebca
decanton <owuordickens@gmail.com>
2023-02-27 02:25:56 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - functions to swap integers
+ * @a: first integer
+ * @b: second integer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
