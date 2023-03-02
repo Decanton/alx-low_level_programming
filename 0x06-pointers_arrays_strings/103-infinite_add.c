f05db7eed35c7fd3f99d92edd157002fefff5421
decanton <owuordickens@gmail.com>
2023-03-02 00:23:55 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,46 +1,46 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * infinite_add - adds two numbers stored as strings
- * @n1: the first number as a string
- * @n2: the second number as a string
- * @r: the buffer to store the result
- * @size_r: the size of the buffer
- * Return: pointer to the result, or 0 if result cannot be stored in r
+ * infinite_add - adds two numbers
+ * @n1: first number to be added
+ * @n2: second number to be added
+ * @r: buffer to store the result
+ * @size_r: size of buffer r
+ * Return: pointer to the result
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0;
-	char *p1 = n1, *p2 = n2, *pr = r;
+	int carry = 0, sum = 0;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int i = len1 - 1, j = len2 - 1, k = 0;
 
-	while (*p1++)
-	len1++;
-	while (*p2++)
-	len2++;
-	if (len1 + 1 > size_r || len2 + 1 > size_r)
-	return (0);
-	p1--, p2--, *pr++ = '\0';
-	while (len1 || len2)
+	while (i >= 0 || j >= 0 || carry > 0)
 	{
 	sum = carry;
-	if (len1)
-	sum += *--p1 - '0';
-	if (len2)
-	sum += *--p2 - '0';
-	carry = sum / 10;
-	*--pr = sum % 10 + '0';
-	len1--, len2--;
-	}
-	if (carry)
-	{
-	if (size_r == 1)
+	if (i >= 0)
+	sum += n1[i] - '0';
+	if (j >= 0)
+	sum += n2[j] - '0';
+	if (k >= size_r - 1)
 	return (0);
-	*--pr = carry + '0';
+	r[k++] = sum % 10 + '0';
+	carry = sum / 10;
+	i--;
+	j--;
 	}
-	else if (*pr == '\0')
+
+	r[k] = '\0';
+	for (int m = 0, n = k - 1; m < n; m++, n--)
 	{
-	*--pr = '0';
+	char temp = r[m];
+
+	r[m] = r[n];
+	r[n] = temp;
 	}
-	return (pr);
+
+	return (r);
 }
