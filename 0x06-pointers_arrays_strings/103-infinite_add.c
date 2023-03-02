3e55cfc12e23213316d4960cc2730e6ffb3e8b01
decanton <owuordickens@gmail.com>
2023-03-02 00:47:07 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int _strlen(char *s);
+
 /**
  * infinite_add - adds two numbers
  * @n1: first number to be added
@@ -14,33 +16,54 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int carry = 0, sum = 0;
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
+	int len1 = _strlen(n1);
+	int len2 = _strlen(n2);
 	int i = len1 - 1, j = len2 - 1, k = 0;
+	int m = 0, n = k - 1;
 
 	while (i >= 0 || j >= 0 || carry > 0)
 	{
-	sum = carry;
-	if (i >= 0)
-	sum += n1[i] - '0';
-	if (j >= 0)
-	sum += n2[j] - '0';
-	if (k >= size_r - 1)
-	return (0);
-	r[k++] = sum % 10 + '0';
-	carry = sum / 10;
-	i--;
-	j--;
+		sum = carry;
+		if (i >= 0)
+		sum += n1[i] - '0';
+		if (j >= 0)
+		sum += n2[j] - '0';
+		if (k >= size_r - 1)
+		return (0); /* checks if output exceeds buffer */
+		r[k++] = sum % 10 + '0';
+		carry = sum / 10;
+		i--;
+		j--;
 	}
 
 	r[k] = '\0';
-	for (int m = 0, n = k - 1; m < n; m++, n--)
+	while (m < n)
 	{
-	char temp = r[m];
+		char temp = r[m];
 
-	r[m] = r[n];
-	r[n] = temp;
+		r[m] = r[n];
+		r[n] = temp;
+		m++;
+		n--;
 	}
 
 	return (r);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string whose length is to be computed
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
