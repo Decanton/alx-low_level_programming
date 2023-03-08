ff3a08509fb781469eed0180a4368b909d2b8662
decanton <owuordickens@gmail.com>
2023-03-08 05:54:53 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,24 +8,24 @@
  * which may contain the special character '*'
  * Return: 1 if the strings are identical, 0 otherwise
  */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-	return (1);
-	if (*s2 == '*') /* current character in s2 is '*' */
+	if (*s1 == '\0')
 	{
-	while (*s1 != '\0')
-	{
-	if (wildcmp(s1, s2 + 1)) /* try all possible matches */
-	return (1);
-	s1++;
-	}
-	return (wildcmp(s1, s2 + 1)); /* assuming '*' matches empty string */
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
 
-	if (*s1 == *s2) /* current characters in s1 and s2 match */
-	return (wildcmp(s1 + 1, s2 + 1));
-
-	return (0); /* strings are not identical */
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
