4f8ea2a2c9188bc7dab99f0e01232540f796a7f7
decanton <owuordickens@gmail.com>
2023-03-20 09:03:52 -0700

    file name

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints name of file 
- * Return: 0 on success
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
-
